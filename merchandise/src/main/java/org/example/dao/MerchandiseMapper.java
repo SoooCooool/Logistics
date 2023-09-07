a63257292540e9505009f7c85f237a30bcdb0246
simo <zht2334200463@gmail.com>
2023-09-07 08:51:30 +0800

    Add files via upload

---
+++ merchandise/src/main/java/org/example/dao/MerchandiseMapper.java
@@ -0,0 +1,16 @@
+package org.example.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.example.model.Merchandise;
+
+import java.util.List;
+
+public interface MerchandiseMapper {
+    public List<Merchandise> findall();
+    public void insert(Merchandise merchandise);
+    public void delete(String order_id);
+    public void update(Merchandise merchandise);
+    public List<Merchandise> findMerchandisebykindandstate(
+            @Param("kind") String kind,
+            @Param("present_state") String present_state);
+}
