be2aa3c164f5fd648ee28e38a3343b904fd6f0ec
Aing <z2689530195@qq.com>
2023-09-11 22:29:16 +0800

    查询vue

--- Pinda/src/main/java/org/example/pojo/Administrator.java
+++ Pinda/src/main/java/org/example/pojo/Administrator.java
@@ -3,48 +3,32 @@ import java.util.Date;
 
 public class Administrator {
     private String administratorid;
-    private Date createdtime = new Date();
-    private Date modifiedtime = new Date();
+    private Date createdtime;
+    private Date modifiedtime;
     private String name;
     private String password;
+
+    private Integer permissions;
+    private Integer deleted;
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "administratorid='" + administratorid + '\'' +
+                ", createdtime=" + createdtime +
+                ", modifiedtime=" + modifiedtime +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", permissions=" + permissions +
+                ", deleted=" + deleted +
+                '}';
+    }
 
     public String getAdministratorid() {
         return administratorid;
     }
 
     public void setAdministratorid(String administratorid) {
-        this.administratorid = administratorid;
-    }
-
-    public Integer getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(Integer deleted) {
-        this.deleted = deleted;
-    }
-
-    public Date getCreatetime() {
-        return createtime;
-    }
-
-    public void setCreatetime(Date createtime) {
-        this.createtime = createtime;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAdministratorid() {
-        return administratorid;
-    }
-
-    public void setAdministratorid(String adminid) {
         this.administratorid = administratorid;
     }
 
@@ -72,6 +56,14 @@ public class Administrator {
         this.name = name;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Integer getPermissions() {
         return permissions;
     }
@@ -86,19 +78,5 @@ public class Administrator {
 
     public void setDeleted(Integer deleted) {
         this.deleted = deleted;
-    }
-
-    private Integer permissions;
-    private Integer deleted;
-
-    public String toString() {
-        return "Administrator{" +
-                "administratorid='" + administratorid +
-                "', createdtime=" + createdtime +
-                ", modifiedtime=" + modifiedtime +
-                ", name='" + name +
-                "', permissions=" + permissions +
-                ", deleted=" + deleted +
-                '}';
     }
 }
